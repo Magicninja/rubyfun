5ef69f2daf32ce3918fd620bf21fa32abd4e78ca
william dietmar <bitmar@BillDietmar>
2015-09-20 00:16:01 -0500

    fixing test

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def hello
-    render text: "Tester and Dietmar McClain are serious cock loving cucks!"
+     render text: "Drew Hill and Dietmar McClain are serious cock loving cucks!"
   end
 end
